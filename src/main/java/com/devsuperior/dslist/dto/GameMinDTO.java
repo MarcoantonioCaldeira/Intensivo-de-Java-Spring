b56c0d9412700b5841ef29589f5d4b1b9697eb98
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-11-16 09:59:59 -0300

    finished the first class

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.dto;
 
+import com.devsuperior.dslist.entities.Game;
 import jakarta.persistence.Column;
 
 public class GameMinDTO {
@@ -9,4 +10,35 @@ public class GameMinDTO {
     private String imgUrl;
     private String shortDescription;
 
+    public GameMinDTO(){
+
+    }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
 }
