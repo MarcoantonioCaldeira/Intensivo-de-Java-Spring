ebdf6bf0ca4fa156cb7cd62dff8ec2012ce60179
Marco Antonio Caldeira <caldeiran391@gmail.com>
2023-11-17 09:54:10 -0300

    finished to class 2

--- src/main/java/com/devsuperior/dslist/serivces/GameService.java
+++ src/main/java/com/devsuperior/dslist/serivces/GameService.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dslist.serivces;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,9 +17,26 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
         var dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
+        var dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
 }
